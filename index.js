196badabfab56f149cec25bca5c4e5057121d1ad
twashington96 <85420381+twashington96@users.noreply.github.com>
2021-09-07 13:11:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/', function(req, res) {
 
-    res.send('<h3>Hello Node on Heroku</h3> <a href="https://github.com/twashington96/my-heroku-app"target="_blank">GitHub Repo</a>');
+    res.send('<h3>Hello Node on Heroku</h3> <a href="https://github.com/twashington96/my-heroku-app" target="_blank">GitHub Repo</a>');
 
 });
 
